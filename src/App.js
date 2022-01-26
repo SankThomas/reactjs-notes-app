04eb0319b410ee458c3e12f95a85b388800aa039
Thomas Sankara <thomassankara74@gmail.com>
2022-01-26 10:37:57 +0300

    render Form and Notes components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import Form from "./components/Form"
+import Notes from "./components/Notes"
 
 function App() {
+  const [notes, setNotes] = useState("")
+  const [items, setItems] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className="flex flex-col items-center justify-center">
+        <div className="p-5 lg:max-w-4xl lg:mx-auto">
+          <h1 className="text-4xl font-bold text-center lg:text-6xl text-gray-700 mb-10">
+            Notes App
+          </h1>
+
+          <Form
+            notes={notes}
+            setNotes={setNotes}
+            items={items}
+            setItems={setItems}
+          />
+
+          <Notes items={items} />
+        </div>
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
